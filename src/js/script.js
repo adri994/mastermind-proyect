8f69701985346045b75337b6b089b520bee78805
Bruno Aggierni <bruno.aggierni@gmail.com>
2021-05-25 10:28:24 +0100

    button disabled before line completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,96 +1,95 @@
-const COLORLIST = ["red", "blue", "green", "yellow", "orange" , "pink"] 
-let randomColors = []
+const COLORLIST = ['red', 'blue', 'green', 'yellow', 'orange', 'pink']
+const randomColors = []
 let ColorUser = []
 
-const check = document.querySelector("#check")
-// We define 2 variable. 
-//Cell indicates the value of the first position, within the first try. 
-let cell = 0
-// Line indicates the try the users have done. 
-let line = 1
-// This variable, calls every element with class "select"
-const select = document.querySelectorAll(".select")
+const check = document.querySelector('#check')
 
+// This variable, calls every element with class "select"
+const select = document.querySelectorAll('.select')
+
+// We define 2 variable.
+// Cell indicates the value of the first position, within the first try.
+let cell = 0
+// Line indicates the try the users have done.
+let line = 1
 
 // function that "paints" each cell, inside their line.
-const paint = function(event){
-    
-    //if (true), we select each class slot (that equals to each cell of the table). With each slot being a child of each row(with line position). This will return an array.
-    // Then slot[cell position] adds a class to the event.target (returns HTML tag that was clicked), and adds the second class form tag. Then we increase cell by 1. 
+const paint = function (event) {
+  // if (true), we select each class slot (that equals to each cell of the table). With each slot being a child of each row(with line position). This will return an array.
+  // Then slot[cell position] adds a class to the event.target (returns HTML tag that was clicked), and adds the second class form tag. Then we increase cell by 1.
 
-    if (line <= 12) {
-        const slots = document.querySelectorAll(`.row${line}>.slot`)
+  if (line <= 12) {
+    const slots = document.querySelectorAll(`.row${line}>.slot`)
 
-        ColorUser.push(event.target.classList[1])
-    
-        slots[cell].classList.add(event.target.classList[1])
-        cell++;
-    
-        // if every cell is completed, we jump to the next line
-        
-        if((slots.length) === cell){
-            cell =0
-            line++
-        }
-    } else {
-        // we asign value 1 to line. we select each element with class slot, and for each one of them, we remove the class applied, in the second position  
-        line = 1
-        document.querySelectorAll(".slot").forEach(function(td){
-            td.classList.remove(td.classList[1])
-        })
-        
+    ColorUser.push(event.target.classList[1])
+
+    slots[cell].classList.add(event.target.classList[1])
+    cell++
+
+    // if every cell is completed, we jump to the next line
+
+    if ((slots.length) === cell) {
+      cell = 0
+      line++
+      check.disabled = false
+      select.forEach(function (button) {
+        button.removeEventListener('click', paint)
+      })
     }
+  } else {
+    // we asign value 1 to line. we select each element with class slot, and for each one of them, we remove the class applied, in the second position
+    line = 1
+    document.querySelectorAll('.slot').forEach(function (td) {
+      td.classList.remove(td.classList[1])
+    })
+  }
 }
-        //Genera combinación aleatoria
-const androidSelector =function (){
-
-    while(randomColors.length<5){
-
-        const randomChoice= Math.floor (Math.random() * (COLORLIST.length))
-        //este if evita repetición de colores aleatorios en clave generada aleatoriamente
-        if(!randomColors.includes(COLORLIST[randomChoice])){
-            randomColors.push(COLORLIST[randomChoice])
-        }
+// Genera combinación aleatoria
+const androidSelector = function () {
+  while (randomColors.length < 5) {
+    const randomChoice = Math.floor(Math.random() * (COLORLIST.length))
+    // este if evita repetición de colores aleatorios en clave generada aleatoriamente
+    if (!randomColors.includes(COLORLIST[randomChoice])) {
+      randomColors.push(COLORLIST[randomChoice])
     }
-    //selección celdas de combinación aleatoria
-    const androidSlot = document.querySelectorAll(".androidSelector>.select")
-    //añadir clase aleatorio a cada celda
-    androidSlot.forEach(function(element,i){
-       
-        element.classList.add(randomColors[i])
-    }); 
+  }
+  // selección celdas de combinación aleatoria
+  const androidSlot = document.querySelectorAll('.androidSelector>.select')
+  // añadir clase aleatorio a cada celda
+  androidSlot.forEach(function (element, i) {
+    element.classList.add(randomColors[i])
+  })
 }
-//creamos función de comparar
-const compare = function(){
-  //selección celdas que muestran resultado (checkbox>select)  
-    const compareSlot = document.querySelectorAll(`.checkbox${line -1} > div`)
-   //comparación colorUser(colores elegidos por el jugador) con ramdomColors (combinación elegida aleatoriamente) 
-    let test1 = ColorUser.map(function(e){
-        return randomColors.indexOf(e)
-    })
-    //recorremos anterior let con dos variables (elemento, index). Si elemento < 0 el color no está (blanco); si i es igual en randomColors y en ColorUser el elemento y la posición es la misma (negro). Si no se cumple nada de esto, el color está pero no en la posición (gris)
-    const test2 = test1.map(function(e, i){
-        if( e < 0){
-            return "white"
-        }else if ( randomColors[i] === ColorUser[i]){
-            return "black"
-        } return "grey"
-    })
-    
-compareSlot.forEach(function(element, index){
+// creamos función de comparar
+const compare = function () {
+  // selección celdas que muestran resultado (checkbox>select)
+  const compareSlot = document.querySelectorAll(`.checkbox${line - 1} > div`)
+  // comparación colorUser(colores elegidos por el jugador) con ramdomColors (combinación elegida aleatoriamente)
+  const test1 = ColorUser.map(function (e) {
+    return randomColors.indexOf(e)
+  })
+  // recorremos anterior let con dos variables (elemento, index). Si elemento < 0 el color no está (blanco); si i es igual en randomColors y en ColorUser el elemento y la posición es la misma (negro). Si no se cumple nada de esto, el color está pero no en la posición (gris)
+  const test2 = test1.map(function (e, i) {
+    if (e < 0) {
+      return 'white'
+    } else if (randomColors[i] === ColorUser[i]) {
+      return 'black'
+    } return 'grey'
+  })
+
+  compareSlot.forEach(function (element, index) {
     element.classList.add(test2[index])
+  })
+
+  ColorUser = []
+}
+
+// for each time we click an element with class select, we apply the function paint
+
+select.forEach(function (element) {
+  element.addEventListener('click', paint)
 })
 
-ColorUser =[]
-
-}
-
-// for each time we click an element with class select, we apply the function paint  
-
-select.forEach(function(element) {
-    element.addEventListener("click",paint)
-});
-
-check.addEventListener("click",compare)
+check.addEventListener('click', compare)
 
 androidSelector()
